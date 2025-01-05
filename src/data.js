ff5f91a9eb0f22db50f8cf17474a3652c123d134
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-05 20:30:41 -0300

    Reorganize the file

--- src/data.js
+++ src/data.js
@@ -136,33 +136,3 @@ const data = [
     },
   },
 ];
-
-function getBooks() {
-  return data;
-}
-
-function getBook(id) {
-  return data.find((d) => d.id === id);
-}
-
-const book = getBook(1);
-
-// const title = book.title;
-// const author = book.author;
-
-const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
-  book;
-
-// console.log(title, author, genres);
-
-// const primaryGenre = genres[0];
-// const secondaryGenre = genres[1];
-
-const [primaryGenre, secondaryGenre, ...otherGenres] = genres;
-// console.log(primaryGenre, secondaryGenre, otherGenres);
-
-const newGenres = [...genres, "epic fantasy"];
-
-const updatedBook = { ...book, moviePublicationDate: "2001-12-19" };
-
-console.log(updatedBook);
