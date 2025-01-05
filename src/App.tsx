da2085b252c7d4a3a62d9bd3f487cfa8f08a1bff
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-04 22:50:21 -0300

    Commiting and getting better

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+import { getBook, getBooks } from "./data";
 function App() {
+  console.log("alou");
+
+  const [braum, setBraum] = useState(getBooks());
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and leo save to reload.
         </p>
         <a
           className="App-link"
