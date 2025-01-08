2a702c89433e2a9e5c21d720367817fc145c0329
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-08 12:40:05 -0300

    Remaking index

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <p>Hello App</p>;
+}
+const rootElement =
+  document.getElementById("root") || document.createElement("div");
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />{" "}
+  </React.StrictMode>,
+);
