ea290115a41550a047e7fef74c0c8c36a7871adb
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-09 17:58:20 -0300

    Pass props

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import { log } from "console";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -69,13 +70,34 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients=" Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients=" Tomato, mozarella, mushrooms, and onion"
+        photoName="pizzas/funghi.jpg"
+        price={15}
+      />
     </main>
   );
 }
-
+function Pizza(props: any) {
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
+  );
+}
 function Footer() {
   return (
     <footer className="footer">
@@ -83,15 +105,7 @@ function Footer() {
     </footer>
   );
 }
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p> Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
