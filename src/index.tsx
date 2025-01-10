7a471ec2ce930f9db5691e47ec088b5202a3d5eb
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-10 19:10:57 -0300

    Challenge concluded

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import "./style.css";
 function App() {
   return (
     <div className="card">
-      {/* <Avatar /> */}
+      <Avatar imgFile={"profile.jpg"} altName={"Niggaheart"} />
       <div className="data">
-        {/* <Intro /> */}
+        <Intro />
         {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-        {/* <SkillList /> */}
+        <SkillList />
       </div>
     </div>
   );
@@ -19,6 +19,35 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 
+function Avatar(props: any) {
+  return <img className={"avatar"} src={props.imgFile} alt={props.altName} />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Terry A. Davis</h1>
+      <p>
+        Terrence Andrew Davis (December 15, 1969 – August 11, 2018) was an
+        American electrical engineer and computer programmer best known for
+        creating and designing TempleOS, an operating system in the public
+        domain, by himself.
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill={"TempleOS"} />
+      <Skill skill={"TempleOS"} />
+      <Skill skill={"TempleOS"} />
+    </div>
+  );
+}
+function Skill(props: any) {
+  return <div className="skill">{props.skill}</div>;
+}
 root.render(
   <React.StrictMode>
     <App />{" "}
