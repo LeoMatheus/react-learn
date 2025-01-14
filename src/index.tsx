85c722a3527eb0d4b922b29b4b1a5850b565aec0
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-14 10:42:06 -0300

    Finalizing challenge

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./style.css";
+
+const skillData = [
+  {
+    name: "JavaScript",
+    color: "#f0db4f",
+    emoji: "🟡",
+  },
+  {
+    name: "Python",
+    color: "#3572A5",
+    emoji: "🐍",
+  },
+  {
+    name: "Java",
+    color: "#B07216",
+    emoji: "☕",
+  },
+  {
+    name: "C++",
+    color: "#f34b7d",
+    emoji: "🟢",
+  },
+  {
+    name: "C#",
+    color: "#239120",
+    emoji: "🅕",
+  },
+  {
+    name: "HTML",
+    color: "#E34C26",
+    emoji: "🌐",
+  },
+  {
+    name: "CSS",
+    color: "#264de4",
+    emoji: "🎨",
+  },
+  {
+    name: "React",
+    color: "#61dafb",
+    emoji: "⚛️",
+  },
+  {
+    name: "Node.js",
+    color: "#68a063",
+    emoji: "🟢",
+  },
+  {
+    name: "SQL",
+    color: "#c0392b",
+    emoji: "💾",
+  },
+];
+
 function App() {
   return (
     <div className="card">
@@ -39,14 +93,25 @@ function Intro() {
 function SkillList() {
   return (
     <div className="skill-list">
-      <Skill skill={"TempleOS"} />
-      <Skill skill={"TempleOS"} />
-      <Skill skill={"TempleOS"} />
+      {skillData.map((skill) => (
+        <Skill skillObj={skill} key={skill.name} />
+      ))}
     </div>
   );
 }
-function Skill(props: any) {
-  return <div className="skill">{props.skill}</div>;
+function Skill({ skillObj }: any) {
+  return (
+    <div
+      className="skill"
+      style={{
+        background: skillObj.color,
+        border: "1px solid rgba(0, 0, 0, 0.75)",
+      }}
+    >
+      <p>{skillObj.name}</p>
+      <span>{skillObj.emoji}</span>
+    </div>
+  );
 }
 root.render(
   <React.StrictMode>
