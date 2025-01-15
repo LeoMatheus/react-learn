d5645f1adda31daf085f2c3dec13be1f952a81f6
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-15 20:43:18 -0300

    Separating components
    To show they are isolated while being the same

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,16 @@ const messages = [
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
+
 export default function App() {
+  return (
+    <div>
+      <Steps />
+      <Steps />
+    </div>
+  );
+}
+function Steps() {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
 
@@ -17,7 +26,7 @@ export default function App() {
     if (step < 3) setStep((s) => s + 1);
   }
   return (
-    <>
+    <div>
       <button className="close" onClick={() => setIsOpen((is) => !is)}>
         &times;
       </button>
@@ -48,6 +57,6 @@ export default function App() {
           </div>
         </div>
       )}
-    </>
+    </div>
   );
 }
