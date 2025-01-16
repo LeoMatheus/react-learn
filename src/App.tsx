43aa978694d7f4385a76e54474797f69c25fa81e
Leo Matheus Raifur <leomraifur@gmail.com>
2025-01-16 19:42:33 -0300

    Step and Counter
    Build step and counter and styled then on my way.
    Next step is building the time  from today

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./index.css";
 export default function App() {
   return (
-    <div>
+    <div className="container">
       <Counter />
     </div>
   );
 }
 function Counter() {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  function incrementCount() {
+    setCount((c) => c + step);
+  }
+
+  function decrementCount() {
+    setCount((c) => c - step);
+  }
+
+  function incrementStep() {
+    setStep((c) => c + 1);
+  }
+
+  function decrementStep() {
+    setStep((c) => c - 1);
+  }
+  return (
+    <div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button className="button" onClick={decrementStep}>
+          -
+        </button>
+        <h1 className="h1">Step: {step}</h1>
+        <button className="button" onClick={incrementStep}>
+          +
+        </button>
+      </div>
+
+      <div style={{ display: "flex" }}>
+        <button className="button" onClick={decrementCount}>
+          -
+        </button>
+        <h1 className="h1">Counter: {count}</h1>
+        <button className="button" onClick={incrementCount}>
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
