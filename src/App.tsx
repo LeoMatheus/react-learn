5629c6ac678c8199ba858b38c2c46f12b3b7f918
Leo Matheus Raifur <leomraifur@gmail.com>
2025-02-04 11:00:35 -0300

    Organize Challenge
    Oragnizing the challenge so i can make them

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,47 @@
 export default function App() {
   return (
-    <div className="app">
-      <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
+    <div className="App">
+      <FlashCards />
     </div>
   );
 }
-
-function Logo() {
-  return <h1>Far Away</h1>;
-}
-function Form() {
+const questions = [
+  {
+    id: 3457,
+    question: "What language is React based on?",
+    answer: "JavaScript",
+  },
+  {
+    id: 7336,
+    question: "What are the building blocks of React apps?",
+    answer: "Components",
+  },
+  {
+    id: 8832,
+    question: "What's the name of the syntax we use to describe a UI in React?",
+    answer: "JSX",
+  },
+  {
+    id: 1297,
+    question: "How to pass data from parent to child components?",
+    answer: "Props",
+  },
+  {
+    id: 9103,
+    question: "How to give components memory?",
+    answer: "useState hook",
+  },
+  {
+    id: 2002,
+    question:
+      "What do we call an input element that is completely synchronised with state?",
+    answer: "Controlled element",
+  },
+];
+function FlashCards() {
   return (
-    <div className="add-form">
-      <h3> What do you need for your trip?</h3>
+    <div>
+      <h1>Leo</h1>
     </div>
-  );
-}
-function PackingList() {
-  return <div className="list">List</div>;
-}
-
-function Stats() {
-  return (
-    <footer className="stats">
-      You have X items on your list, and already packed X (X%)
-    </footer>
   );
 }
